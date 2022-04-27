aa3af22705a3264692465072b6b2774b0ee22094
Gearmo <93457954+Gearmo@users.noreply.github.com>
2022-04-27 14:11:07 -0700

    Add files via upload

---
+++ Torch Of The Depths PLANNING.js
@@ -0,0 +1,35 @@
+/* idea for game
+
+A single player game that allows you to start as one of X amount of classes, each with perks.
+
+// PLAYER PERKS
+HP: health points
+MANA: a mana point system that allows for certain spells to be used
+STRENGTH: attack damage
+MAGIC: spell damage
+PRECISION: odds of landing YOUR attacks 
+DEXTERITY: odds of dodging ENEMY attacks
+LUCK: odds of finding loot along journey
+LOOTING: odds of finding loot after battle
+
+[LVL] 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+[MNA] 
+[STR] 
+[MAG] 
+[PRC] 
+[DEX] 
+[LCK] 
+[LTG] 
+
+Each class will start at Lvl 5 and can level up via battling or finding loot. You are also assigned a buddy at the start, which has a chance to help during different stages of the game.
+
+// -------------------------FLOW----------------------------- //
+
+JavaScript Prompt: Choose a Class: A, B, C
+JavaScript Prompt: Choose Gender: M, F, TT
+JavaScript Prompt: Choose Buddy: A, B, C
+
+Show character w/two buttons: Start Adventure, or Pick New Hero (will simply refresh page). 
+
+*/
+
